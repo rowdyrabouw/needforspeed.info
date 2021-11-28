8a7ee9b599298e9fe7887b0629edfb2158cfe6a7
Rowdy Rabouw <rowdy@double-r.nl>
2021-11-28 14:07:15 +0100

    feat: add Service Worker

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,10 @@
-// TODO: register Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then(() => {
+      console.log("%c[app.js] Service Worker registered", "color: #5B9B4C");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
