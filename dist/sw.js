53a2d23911a2eb5697014e5d237f9effcade43b8
Rowdy Rabouw <rowdy@double-r.nl>
2021-11-28 14:19:29 +0100

    feat: add static cache and refresh Service Worker after install

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,26 @@
+const STATIC_CACHE = "static-v1";
+const STATIC_FILES = [
+  "/",
+  "index.html",
+  "/favicon.png",
+  "assets/css/style.css",
+  "assets/fonts/nfs.woff2",
+  "assets/fonts/din_alternate.woff",
+  "assets/img/need-for-speed-1.jpg",
+  "assets/js/app.js",
+];
+
 self.addEventListener("install", (event) => {
   console.log("%c[sw.js] Service Worker installed", "color: #FEC233");
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => {
+      cache.addAll(STATIC_FILES).then(() => {
+        console.log("%c[sw.js] Files added to static cache", "color: #FEC233");
+        self.skipWaiting();
+        window.location.reload();
+      });
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
